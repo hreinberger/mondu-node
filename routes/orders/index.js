2d97fafb2623189668480886da7afc7882d03e66
hreinberger <40197225+hreinberger@users.noreply.github.com>
2023-03-22 16:31:54 +0100

    ui improvements

--- routes/orders/index.js
+++ routes/orders/index.js
@@ -2,10 +2,10 @@ var express = require("express");
 var router = express.Router();
 var axios = require("axios");
 
-let getOrders = async () => {
+let getOrders = async (page) => {
   const config = {
     method: "get",
-    url: "https://api.demo.mondu.ai/api/v1/orders?page=1&per_page=10",
+    url: `https://api.demo.mondu.ai/api/v1/orders?page=${page}&per_page=10`,
     headers: { "Api-Token": process.env.MONDU_KEY },
   };
 
@@ -13,10 +13,18 @@ let getOrders = async () => {
   return response.data.orders;
 };
 
-router.get("/", async (req, res, next) => {
-  orders = await getOrders();
+router.get("/:page?", async (req, res, next) => {
+  const page = req.params.page ? parseInt(req.params.page) : 1;
+
+  // Validate that page is an integer
+  if (!Number.isInteger(page)) {
+    return res.status(400).send("Invalid page number");
+  }
+
+  orders = await getOrders(page);
   res.render("orders", {
     orders,
+    page,
   });
 });
 
