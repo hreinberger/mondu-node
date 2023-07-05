248b6e66b53d159c2eb2f8b9c057bcfb9769bf21
Jaku6Rojek <15034779+Jaku6Rojek@users.noreply.github.com>
2023-07-05 15:15:07 +0200

    removing console.log from orderStatusWebhook

--- routes/orderStatusWebhook.js
+++ routes/orderStatusWebhook.js
@@ -6,13 +6,7 @@ const upload = multer();
 var handleWebhookPayload = require("../public/javascripts/handleWebhookPayload.js");
 
 router.post("/", upload.none(), async function (req, res) {
-  //log incoming order data
-  console.log(req.body);
-  console.log(req.get('host'));
-  console.log(req.originalUrl);
   var result = handleWebhookPayload.storeWebhooksPayload(req.body);
-  console.log('result:', result);
-  console.log('get payload:', handleWebhookPayload.getWebhooksPayload());
   res.sendStatus(200);
 });
 
